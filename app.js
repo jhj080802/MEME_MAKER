4ad54808ac4840ff3050d6c5e9737ca3ccbefee0
hyojoo <jhj080802@naver.com>
2024-04-25 19:26:55 +0900

    #1.2 Paths

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 700;
 
+ctx.rect(50,50,100,100);
 ctx.rect(150,150,100,100);
 ctx.rect(250,250,100,100);
 ctx.fill();
 
 ctx.beginPath();
 ctx.rect(350,350,100,100);
-ctx.rect(450,450,100,100);
 ctx.fillStyle = "red";
-setTimeout(() => {
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(450,450,100,100);
+ctx.fillStyle = "green"
+setTimeout(()=>{
     ctx.fill();
-}, 5000);
+}, 3000);
+
