61561adb5b20650b515a9c0abe2d92fce45fffe0
hyojoo <jhj080802@naver.com>
2024-04-25 17:19:09 +0900

    #1.2 Paths

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const canvas = document.querySelector("canvas");
 
-const context = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 500;
+const ctx = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 700;
 
-context.strokeRect(50,50,100,200);
+ctx.rect(150,150,100,100);
+ctx.rect(250,250,100,100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(350,350,100,100);
+ctx.rect(450,450,100,100);
+ctx.fillStyle = "red";
+setTimeout(() => {
+    ctx.fill();
+}, 5000);
