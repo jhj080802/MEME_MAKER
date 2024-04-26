9d0e1c903457f3c9a85e4d933044e5f6d09c4f53
hyojoo <jhj080802@naver.com>
2024-04-26 10:41:56 +0900

    #2.3 Paint Color part One

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -24,8 +25,12 @@ function cancelPainting(){
 }
 
 function onLineWidthChange(event){
-    ctx.lineWidth=event.target.value;
+    ctx.lineWidth=event.target.value;   
+}
 
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle=event.target.value;    
 }
 
 canvas.addEventListener("mousemove",onMove);
@@ -35,3 +40,4 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
