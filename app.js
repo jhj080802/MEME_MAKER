b286ac9470c9237e9e30f743a8d9329cf5810d99
hyojoo <jhj080802@naver.com>
2024-04-26 09:14:54 +0900

    #2.1 Mouse Painting

--- app.js
+++ app.js
@@ -3,30 +3,27 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
-// https://flatuicolors.com/
-const colors = [
-    "#ff9ff3",
-    "#ee5253",
-    "#48dbfb",
-    "#00d2d3",
-    "#5f27cd",
-    "#c8d6e5",
-    "#feca57",
-    "#2e86de",
-    "#f368e0",
-]
-
 ctx.lineWidth = 2;
+let isPainting = false;
 
-function onclick(event){
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-// canvas.addEventListener("click", onclick);
-canvas.addEventListener("mousemove", onclick);
+function startPainting(){
+    isPainting = true;
+}
+
+function cancelPainting(){
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove",onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
