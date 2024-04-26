ed69101b3961f3e35d9fbde713c4d7a81ea3cb30
hyojoo <jhj080802@naver.com>
2024-04-26 09:34:51 +0900

    #2.2 Line Width

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
-
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event){
@@ -12,18 +12,26 @@ function onMove(event){
         ctx.stroke();
         return;
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
 function startPainting(){
     isPainting = true;
 }
-
 function cancelPainting(){
     isPainting = false;
+}
+
+function onLineWidthChange(event){
+    ctx.lineWidth=event.target.value;
+
 }
 
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+
+lineWidth.addEventListener("change", onLineWidthChange);
