d7154e92b255130a8a736ef44328a3314529b796
hyojoo <jhj080802@naver.com>
2024-04-29 20:20:14 +0900

    #3.1 Adding Text

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const textInput = document.getElementById("text");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const fileInput = document.getElementById("file");
@@ -17,6 +18,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap="round";
 let isPainting = false;
 let isFilling = false;
 
@@ -89,7 +91,18 @@ function onFileChange(event){
         ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     };
 }
+function onDoubleClick(event){
+    const text = textInput.value;
+    if(text!==""){
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "68px serif";
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
 
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
